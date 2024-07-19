5a8e5114f546e7d6669db4f64bbaeab382447e28
mwammwa <147014227+mwammwa@users.noreply.github.com>
2024-07-19 11:34:18 +0900

    Add files via upload

---
+++ AppleMarket/app/src/main/java/com/example/applemarket/ItemList.kt
@@ -0,0 +1,16 @@
+package com.example.applemarket
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class MyItem(
+    val listImage: Int,
+    val listTitle: String,
+    val listAddress: String,
+    val listPrice: String,
+    val chatCount: Int,
+    var likeCount: Int,
+    val nickname: String,
+    val detailContent: String
+) : Parcelable
